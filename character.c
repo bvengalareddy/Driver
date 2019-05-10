1194cfb81b7806902306465efd423970445c7f0f
vengalareddyb <vengala.rao0800@cientra.com>
2019-05-10 10:07:04 +0530

    Character driver updation
    Source:www.cientra.com
    
    MR:1235
    
    Type:Adding
    
    Dispotion:Linux kernel version 2.6
    
    Description:
    	add the device into the /dev.
    
    Signed-off-by Vengala Reddy <vengala.rao0800@cientra.com>

--- character.c
+++ character.c
@@ -5,6 +5,11 @@
 #include <linux/poll.h>
 #include <linux/ioport.h>
 #include <linux/errno.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/kobject.h>
+#include <linux/sysfs.h>
+#include <linux/kdev_t.h>
 
 MODULE_AUTHOR("Vengala Reddy");
 MODULE_LICENSE("GPL");
@@ -18,23 +23,28 @@ static ssize_t mydev_write(struct file *file, const char *buf, size_t count,
 
 
 #define  MAX_BUF_SIZE  1024
-
+dev_t dev=0;
 typedef struct 
 {
 	char buf[1024];
 	int noOfChars;
 }mydev_t;
 
+	 struct class *dev_class;
+         struct cdev dev_cdev;
+	struct kobject *kobj_ref;
 mydev_t mydev;
 
 #define MYDEV_MAJOR_NUM   42
 #define MYDEV_NAME        "mydev"
 
+
 static struct file_operations mydev_ops =
 {
-read:mydev_read,
-     write:mydev_write,
-     open:mydev_open,
+     owner : THIS_MODULE,	
+     read  :mydev_read,
+     write :mydev_write,
+     open  :mydev_open,
      release:mydev_close,
 };
 
@@ -55,9 +65,39 @@ int mydev_init(void)
 	{
 		printk("<1>" "Registration success %d\n",res);
 	}
-	mydev.noOfChars=0;
+//	mydev.noOfChars=0;
 
-	return 0;
+//	printk(KERN_INFO "Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
+
+        /*Creating cdev structure*/
+        cdev_init(&dev_cdev,&mydev_ops);
+
+        /*Adding character device to the system*/
+        if((cdev_add(&dev_cdev,res,1)) < 0){
+            printk(KERN_INFO "Cannot add the device to the system\n");
+            goto r_class;
+        }
+
+        /*Creating struct class*/
+        if((dev_class = class_create(THIS_MODULE,"mydev_class")) == NULL){
+            printk(KERN_INFO "Cannot create the struct class\n");
+            goto r_class;
+        }
+
+        /*Creating device*/
+        if((device_create(dev_class,NULL,res,NULL,"mydev_device")) == NULL){
+            printk(KERN_INFO "Cannot create the Device 1\n");
+            goto r_device;
+        }
+
+
+r_device:
+        class_destroy(dev_class);
+r_class:
+        unregister_chrdev_region(res,1);
+        cdev_del(&dev_cdev);
+        return -1;
+
 }
 
 /*******************************************************************************
@@ -66,6 +106,9 @@ int mydev_init(void)
  *******************************************************************************/
 void mydev_cleanup(void)
 {
+	 kobject_put(kobj_ref);
+//	  device_destroy(mydev->dev_class,res);
+	   cdev_del(&dev_cdev);
 	unregister_chrdev(MYDEV_MAJOR_NUM, MYDEV_NAME);
 	printk(KERN_ALERT "good bye\n");
 
