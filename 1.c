5a1be71ad83447ee555efc1d22042f61b4c4425b
vengalareddy <vengala.rao0800@cientra.com>
2019-05-15 14:33:10 +0530

    bitwise operator
    Source:www.cientra.com
    
    MR:12345
    
    Type:adding
    
    Dispotion:greekforce
    
    Description:
    	Bitwise operator negaotion.
    
    Signed-off-by vengala rao <vengala.rao0800@cientra.com>

--- 1.c
+++ 1.c
@@ -1,31 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
 
 int main()
 {
-     printf("wpa_passphrase is loading...\n");
-     printf("\n");
+	int x=0;
+	int k=~(x);
 
-	(void)system("/home/bhogireddi/Desktop/Backup_Data/Home/hostap/wpa_supplicant/wpa_passphrase MTPL-1 MTPL#MTPL#1");
-       printf("\n");
-	printf("\n");
-	printf("wpa_gui is loading\n");
-	printf("\n");
+	printf("%d\n",k);
 
-	(void)system("/home/bhogireddi/Desktop/Backup_Data/Home/hostap/wpa_supplicant/wpa_gui-qt4/wpa_gui");
-     printf("\n");
-     printf("\n");
-     printf("wpa_supplicant loading\n");
-     printf("\n");
-
-	(void)system("/home/bhogireddi/Desktop/Backup_Data/Home/hostap/wpa_supplicant/wpa_supplicant -Dnl80211 -iwlp5s0 -c /home/bhogireddi/hostap/hostap/wpa_supplicant/wpa_supplicant.conf");
-
- //    (void)system("/home/bhogireddi/Desktop/Backup_Data/Home/hostap/wpa_supplicant/wpa_supplicant -Dnl80211 -iwlx001ea653a7d3 -c /home/bhogireddi/hostap/hostap/wpa_supplicant/wpa_supplicant.conf");
-     printf("\n");
-     printf("\n");
-     printf("wpa_cli loading\n");
-     printf("\n");
-
-	(void)system("/home/bhogireddi/Desktop/Backup_Data/Home/hostap/wpa_supplicant/wpa_cli");
 }
